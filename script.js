ae0ece6d4ef19781d6dde7bc74e66700138fea35
Mandeep-Heir <mandeepheir11@gmail.com>
2026-05-24 02:23:51 -0400

    Added deposit button functionality

--- script.js
+++ script.js
@@ -2,4 +2,24 @@
 let balance = 5000;
 
 // show balanece in console 
-console.log(balance);
+
+//nsole.log(balance);
+// select banace text element
+let balanceText = document.getElementById("balance");
+// select deposit button 
+
+let depositButton = document.getElementById("deposit");
+
+// show initial balance 
+balanceText.innerText = "$" + balance;
+
+// add event listener to deposit button
+depositButton.addEventListener("click", function() {
+    // Increase Balance 
+
+    balance = balance +100;
+
+    // update balance text
+    balanceText.innerText = "$" + balance;
+});
+
